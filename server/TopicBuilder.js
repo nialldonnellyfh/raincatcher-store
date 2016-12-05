319752c3388e8cb79560b33efe8b5b0ec625fe47
Niall Donnelly <niall.donnelly@feedhenry.com>
2016-12-05 15:58:21 +0000

    Using specific store

--- server/TopicBuilder.js
+++ server/TopicBuilder.js
@@ -1,20 +1,35 @@
+/**
+ *
+ * A raincatcher topic builder for registering topics for storage topics.
+ * @param storeId
+ * @param raincatcherStore
+ * @constructor
+ */
 function TopicBuilder(storeId, raincatcherStore) {
   this.storeId = storeId;
   this.raincatcherStore = raincatcherStore;
   this.prefix = '';
 }
 
+/**
+ * Specifying a prefix for this topic generator.
+ * @param prefix
+ */
 TopicBuilder.prototype.prefix = function(prefix) {
   this.prefix = prefix;
 };
 
+/**
+ * Specifying an entity for this topic generator
+ * @param entityId
+ */
 TopicBuilder.prototype.entity = function(entityId) {
   this.entityId = entityId;
 };
 
 /**
  *
- * Creating a subscriber for the top
+ * Creating a subscriber for the topic in the raincatcher store.
  *
  * @param topic
  * @param topicFn
